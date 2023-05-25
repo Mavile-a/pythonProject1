52856265e7cd07c35032151c403235dbc418a051
Mavile Alieva <mavile.alieva.01@gmail.com>
2023-05-25 15:04:31 +0300

    else

--- selli.py
+++ selli.py
@@ -2,12 +2,12 @@ user = int(input('-->'))
 print(type(user))
 
 if user < 6:
-    print('Де твої батьки?')
+    print('Where are your parents?')
 elif user < 16:
     print('This is a movie for adults!')
 elif user > 65:
     print('Show your pension certificate!')
 elif '7' in 'user':
     print('You will be lucky!')
-
+else:
     print('There are no more tickets!')
