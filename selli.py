31e137785d3173e62ba00b0a39a42b596a8905f7
Mavile Alieva <mavile.alieva.01@gmail.com>
2023-05-25 14:49:21 +0300

    Initial commit

---
+++ selli.py
@@ -0,0 +1,13 @@
+user = int(input('-->'))
+print(type(user))
+
+if user < 6:
+    print('Де твої батьки?')
+elif user < 16:
+    print('This is a movie for adults!')
+elif user > 65:
+    print('Show your pension certificate!')
+elif '7' in 'user':
+    print('You will be lucky!')
+
+    print('There are no more tickets!')
